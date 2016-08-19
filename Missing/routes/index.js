c737e3e787b29a532752d01a64d1635c5f000bdd
fahrurrozzi <izzor.master@gmail.com>
2016-08-19 22:51:59 +0900

    connect mongodb

--- Missing/routes/index.js
+++ Missing/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require ('mongoose');
+// var MemberScheme = mongoose.Schema({username : String, nickname : String});
+var Member = mongoose.model('MemoModel', MemberScheme);
+mongoose.connect('mongodb://localhost/mongotest', function (err) {
+  if (err){
+    console.log('mongoose connection error: '+err);
+    throw err;
+  }
+  console.log('mongoose connection success');
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
