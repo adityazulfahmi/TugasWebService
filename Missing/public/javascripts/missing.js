f4d40b762f3017273c76b5f783718e3ccd5b0aa5
fahrurrozzi <izzor.master@gmail.com>
2016-08-20 04:02:49 +0900

    add router to get all people in db

---
+++ Missing/public/javascripts/missing.js
@@ -0,0 +1,46 @@
+/**
+ * Created by ajou on 8/19/2016.
+ */
+var module = angular.module('missingApp', []);
+module.controller('MainCtrl', ['$scope', '$http' , function ($scope, $http) {
+    // $scope.user ={};
+    // $scope.user.username ='';
+    $scope.submit = function () {
+        var date = new Date();
+        $scope.year = date.getFullYear();
+
+        $http({
+            method : 'POST',
+            url : 'http://localhost:3000/add',
+            data : {
+               // photo   : $scope.photo,
+                name    : $scope.name,
+                dob     : $scope.dob,
+                pob     : $scope.pob,
+                hair    : $scope.hair,
+                eyes    : $scope.eyes,
+                height  : $scope.height,
+                weight  : $scope.weight,
+                sex     : $scope.sex,
+                race    : $scope.race,
+                sam     : $scope.sam,
+                reward  : $scope.reward,
+                remarks : $scope.remarks,
+                details : $scope.details,
+                contact : $scope.contact,
+                year : $scope.year
+            }
+        })
+            .success(function(data, status, headers, config) {
+                if( data ) {
+                    alert(data);
+                    //$scope.members= data;
+                }
+                else {
+                }
+            })
+            .error(function(data, status, headers, config) {
+                console.log(status);
+            });
+    };
+}]);
--- Missing/routes/index.js
+++ Missing/routes/index.js
@@ -32,6 +32,7 @@ mongoose.connect('mongodb://localhost/missingdb', function (err) {
 })
 
 router.post('/add', function(req, res, next) {
+  console.log("woy");
   var photo   = req.body.photo;
   var name    = req.body.name;
   var dob     = req.body.dob;
@@ -49,8 +50,27 @@ router.post('/add', function(req, res, next) {
   var contact = req.body.contact;
   var year = req.body.year;
 
+  console.log(
+      "photo :"+photo+"\n"+
+      "name  :"+name+"\n"+
+      "dob   :"+dob+"\n"+
+      "pob   :"+pob+"\n"+
+      "hair  :"+hair+"\n"+
+      "eyes  :"+eyes+"\n"+
+      "height:"+height+"\n"+
+      "weight:"+weight+"\n"+
+      "sex   :"+sex+"\n"+
+      "race  :"+race+"\n"+
+      "sam   :"+sam+"\n"+
+      "reward:"+reward+"\n"+
+      "remarks:"+remarks+"\n"+
+      "details:"+details+"\n"+
+      "contact:"+contact+"\n"+
+      "year  :"+year
+  );
+
   var person = new Missing({
-    photo : fs.readFileSync(photo),
+    //photo : fs.readFileSync(photo),
     name  : name,
     dob   : dob,
     pob   : pob,
@@ -67,6 +87,8 @@ router.post('/add', function(req, res, next) {
     contact:contact,
     year  :year
   });
+
+  //alert("lalalal"+JSON.parse(person));
   person.save(function (err, silence) {
     if(err){
       console.log(err);
@@ -76,6 +98,92 @@ router.post('/add', function(req, res, next) {
     res.send('success');
   });
 });
+
+router.get('/people', function(req, res, err) {
+
+  var people = new Missing();
+
+  Missing.find(function (err, people) {
+    if(err)
+      return res.status(500).send({error : 'database Failure'});
+    console.log(people);
+    res.json(people);
+  })
+});
+
+// router.get('/people/:id', function(req, res, err) {
+//
+//   var person = new Missing();
+//
+//   Missing.findOne({_id: req.params.id},function (err, person) {
+//     if(err) {
+//       console.err(err);
+//       throw err;
+//     }
+//     console.log(person);
+//     res.json(person);
+//   })
+// });
+//
+// //year
+// router.get('/people/:year', function(req, res, err) {
+//
+//   var people = new Missing();
+//
+//   Missing.findOne({year: req.params.year},function (err, people) {
+//     if(err) {
+//       console.err(err);
+//       throw err;
+//     }
+//     console.log(people);
+//     res.json(people);
+//   })
+// });
+//
+// //sex
+// router.get('/people/:sex', function(req, res, err) {
+//
+//   var people = new Missing();
+//
+//   Missing.findOne({sex: req.params.sex},function (err, people) {
+//     if(err) {
+//       console.err(err);
+//       throw err;
+//     }
+//     console.log(people);
+//     res.json(people);
+//   })
+// });
+//
+// //name
+// router.get('/people/:name', function(req, res, err) {
+//
+//   var people = new Missing();
+//
+//   Missing.findOne({name: req.params.name},function (err, people) {
+//     if(err) {
+//       console.err(err);
+//       throw err;
+//     }
+//     console.log(people);
+//     res.json(people);
+//   })
+// });
+//
+// //reward
+// router.get('/people/reward', function(req, res, err) {
+//
+//   var people = new Missing();
+//
+//   Missing.findOne({_id: req.params.year},function (err, people) {
+//     if(err) {
+//       console.err(err);
+//       throw err;
+//     }
+//     console.log(people);
+//     res.json(people);
+//   })
+// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
